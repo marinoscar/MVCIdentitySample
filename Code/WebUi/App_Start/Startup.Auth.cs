c64ad857e0cdf5de53ca7f56099cac56269dfdc7
Oscar Marin <oscar.marin@entegreat.com>
2014-08-19 20:18:08 -0600

    Added references and initial code

---
+++ Code/WebUi/App_Start/Startup.Auth.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using Microsoft.Owin;
+using Microsoft.Owin.Security;
+using Microsoft.Owin.Security.Cookies;
+using Microsoft.Owin.Security.Infrastructure;
+using Microsoft.Owin.Security.OAuth;
+using Owin;
+using WebUi.App_Start;
+
+[assembly: OwinStartup(typeof(Startup))]
+
+namespace WebUi.App_Start
+{
+	public partial class Startup
+	{
+		public void Configuration(IAppBuilder app)
+		{
+		    ConfigureAuth(app);
+		}
+
+        public void ConfigureAuth(IAppBuilder app)
+        {
+            // Enable the Application Sign In Cookie.
+            app.UseCookieAuthentication(new CookieAuthenticationOptions
+            {
+                AuthenticationType = "Application",
+                AuthenticationMode = AuthenticationMode.Passive,
+                LoginPath = new PathString("/Account/Login"),
+                LogoutPath = new PathString("/Account/Logout"),
+            });
+
+            // Enable the External Sign In Cookie.
+            app.SetDefaultSignInAsAuthenticationType("External");
+            app.UseCookieAuthentication(new CookieAuthenticationOptions
+            {
+                AuthenticationType = "External",
+                AuthenticationMode = AuthenticationMode.Passive,
+                CookieName = CookieAuthenticationDefaults.CookiePrefix + "External",
+                ExpireTimeSpan = TimeSpan.FromMinutes(5),
+            });
+
+            // Enable Google authentication.
+            app.UseGoogleAuthentication();
+        }
+	}
+}
